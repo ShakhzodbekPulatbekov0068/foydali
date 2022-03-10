48f4ae56802acca8f6463cc33270afb208a6d05e
LEGION <Shaxzodbekbest5511@gmail.com>
2022-03-10 19:47:31 +0500

    target

--- src/main/java/pdp_g9/telegram_bot/bot/Main.java
+++ src/main/java/pdp_g9/telegram_bot/bot/Main.java
@@ -305,7 +305,7 @@ public class Main extends TelegramLongPollingBot implements ReadFromExcel {
 
                 case "NUMBEROFUSERS":
                     String string = userService.numberOfUsers();
-                    sendMessage.setText("nmaga kere userlar "+string);
+                    sendMessage.setText(string);
                     sendMessage.setChatId(String.valueOf(chatId));
                     executes2(sendMessage);
                     break;
